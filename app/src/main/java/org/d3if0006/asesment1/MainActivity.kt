1db6ce137e99e700a57ecec694021b002ccbbbed
AyuSuryaDhemira <dhemirasurya15@gmail.com>
2023-03-27 16:15:12 +0700

    Menambahkan view binding

--- app/src/main/java/org/d3if0006/asesment1/MainActivity.kt
+++ app/src/main/java/org/d3if0006/asesment1/MainActivity.kt
@@ -2,10 +2,23 @@ package org.d3if0006.asesment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.d3if0006.asesment1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener { hitungDheLaundry() }
+
+    }
+    private fun hitungDheLaundry() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
